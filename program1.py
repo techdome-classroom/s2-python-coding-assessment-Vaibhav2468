5b327b23c0435fbd461e7eb4e41930cef2bd849c
Vaibhav2468 <vaibhavsarware@gmail.com>
2024-10-07 14:16:42 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -7,17 +7,17 @@ class Solution(object):
         bracket_map = {')': '(', '}': '{', ']': '['}
     
 
-    for char in s:
-        if char in bracket_map:  
-            top_element = stack.pop() if stack else '#'
-            if bracket_map[char] != top_element:
-                return False
-        else:
-            
-            stack.append(char)
-    
-    
-    return not stack
+        for char in s:
+            if char in bracket_map:  
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                
+                stack.append(char)
+        
+        
+        return not stack
 
 
 
